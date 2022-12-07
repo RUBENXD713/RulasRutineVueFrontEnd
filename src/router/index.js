789899127b1bb193de1a2cf27b27c7614a689157
Angel Valdez <ang_endless27@hotmail.com>
2022-12-07 01:02:02 -0600

    [FIX] changes

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,9 @@ import Historial from '../views/viewsRulasRutine/historial.vue'
 import UpdateUser from '../views/viewsRulasRutine/editarPerfil.vue'
 import RegistrarPeso from '../views/viewsRulasRutine/registrarPeso.vue'
 import Felicidades from '../views/viewsRulasRutine/felicidades.vue'
+import Blog from '../views/viewsRulasRutine/Blog.vue'
+import RutinaEjercicio from '@/views/viewsRulasRutine/Rutina-Ejercicio.vue'
+import prueba from '@/views/viewsRulasRutine/prueba.vue'
 
 //other
 import Vue from 'vue'
@@ -109,8 +112,6 @@ const routes = [
     name: 'congratulations',
     component: Felicidades
   },
-
-
 
   //other
   {
